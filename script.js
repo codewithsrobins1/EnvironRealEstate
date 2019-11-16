04e16749c745cec57ef54b22ae09e657b998cbdc
codewithsrobins1 <codewithsrobins1@gmail.com>
2019-11-16 09:14:58 -0800

    111619

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 // Display Home When Hovering Over Card Photo
+// //onmouseover
+// function mouseOver(){
+//   var image = document.getElementById("imgHover");
+//
+//   image.src = "/img/modPage1.jpg"
+// };
+//
+// //onmouseout
+// function noMouseOver(){
+//   var image = document.getElementById("imgHover");
+//
+//   image.src = "/img/modern.jpg"
+// };
+//
+// document.getElementById("imgModern").addEventListener("mouseenter", mouseOver());
+// document.getElementById("imgModern").addEventListener("mouseleave", noMouseOver());
+
+
 //onmouseover
 function mouseOver(){
- document.getElementById("imgHover").src = "/img/modernPage1.jpg";
+  //List the Image ID that will Change the Item when Hovered
+  var hoverItems = []
+
+  //If Hovered Over Modern, Change Image
+  document.getElementById("imgHover").src = "/img/modernPage1.jpg"
+
+  //If Hovered Over Contemporary, Change Image
+
+  //If Hovered Over Mediterranean, Change Image
+
+  //If Hovered Over Luxury, Change Image
+
 };
 
 //onmouseout
 function noMouseOver(){
   document.getElementById("imgHover").src = "/img/modern.jpg"
-}
-
-mouseOver();
-
-document.getElementById("imgModern").addEventListener('hover', mouseOver() );
-
-// https://five-demo.squarespace.com/
+};
